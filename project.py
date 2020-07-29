7819fe628d7a257aaa7d0adbbe8bc769133e47d2
ishitb <ib370@snu.edu.in>
2020-07-30 01:36:29 +0530

    Building a repo with only one python file to convert into exe

--- project.py
+++ project.py
@@ -1,10 +1,8 @@
-import os, shutil, subprocess, sys, json
+import os, shutil, subprocess, sys, json, requests
 from colorama import init 
 from termcolor import colored, cprint
 from utils.github_auto import push_to_github
-from utils.custom_io import print_choices, printc, printe, inputc
 from utils.Project_Creation import native_web, react_web, react_native, flutter, django
-from utils.clear import clear
 
 BASE_PATH = 'C:/Users/Lenovo/Desktop/MyPC/Projects/'
 
@@ -13,13 +11,48 @@ if sys.platform == 'linux' :
 
 THIS_PATH = os.path.dirname(__file__)
 
-COLORS = json.load(open(os.path.join(THIS_PATH, "utils", "Shared", "colors.json")))
+COLORS = [
+    "red",
+    "yellow",
+    "blue",
+    "magenta",
+    "cyan",
+    "white",
+    "grey",
+    "green"
+]
 
 MAIN_COLOR = COLORS[7]
 
 BACKGROUND_COLORS = [f"on_{color}" for color in COLORS ]
 
-PROJECT_BASES = json.load(open(os.path.join(THIS_PATH, 'utils', 'Shared', 'project_bases.json')))
+PROJECT_BASES = [
+    {
+        "name": "Flutter App",
+        "directory": "Android/Flutter_Projects",
+        "color": "red"
+    },
+    {
+        "name": "React Native App",
+        "directory": "Android/ReactNative",
+        "color": "cyan"
+    },
+    {
+        "name": "React Website Project",
+        "directory": "Web/React",
+        "color": "yellow"
+    },
+    {
+        "name": "Basic Native Website Project",
+        "directory": "Web/Native_Web",
+        "color": "blue"
+    },
+    {
+        "name": "Backend Project With Django",
+        "directory": "Backend",
+        "color": "magenta"
+    }
+]
 
 IDE_CHOICES = [
     {
@@ -31,6 +64,35 @@ IDE_CHOICES = [
         'command': 'code .'
     }
 ]
+
+def clear() :
+    os.system('clear')
+
+def printc(string, end='\n') : 
+    cprint(string, MAIN_COLOR, end=end)
+
+def printe(string, end='\n') : 
+    cprint(string, COLORS[0], end=end)
+
+def inputc(string, color=MAIN_COLOR, attrs=[], newLine=False) :
+    newLine = '\n' if newLine else ''
+    try :
+        newInput = input(colored(f"{string} {newLine}> ", color, attrs=attrs))
+
+    except :
+        printe("Interrupted!!")
+        exit(0)
+
+    return newInput
+
+def print_choices(question, choices) :
+    printc(question + '?')
+
+    for choice in range(len(choices)) :
+        printc(choice + 1, end='. ')
+        cprint(choices[choice], COLORS[int(uniform(0, 7))])
+    
+    return int(inputc("Enter your Choice (Enter a Number)"))
 
 def choose_project() :
 
@@ -110,8 +172,17 @@ if __name__ == '__main__' :
     
     clear()
     
+    if not os.path.exists(BASE_PATH) :
+        while True :
+            BASE_PATH = inputc("Please enter the path where you want the Projects folder? (Default - 'Desktop')")
+            if BASE_PATH == "" :
+                BASE_PATH = os.environ['HOME']
+            if not os.path.exists(BASE_PATH) :
+                print("Please print a valid path!")
+                continue
+            break
     os.chdir(BASE_PATH)
-
+    
     print()
     cprint(" N E W ", 'magenta', 'on_white', end='', attrs=['bold'])
     cprint(" P R O J E C T ", 'white', 'on_magenta', attrs=['bold'])
@@ -159,3 +230,120 @@ if __name__ == '__main__' :
 
     else :
         printc(base)
+
+
+# For Github Automation
+from requests.auth import HTTPBasicAuth
+
+def push_to_github(project_name, project_description, git_ignore=[]) :
+    if not wifi() :
+        return False
+    
+    # Check if git is installed
+    installed = subprocess.getoutput('git --version')
+    if installed.split()[0] != 'git' :
+        print("Please install git first and then try! Keeping repository local")
+        return False
+
+    else :        
+        git_init()
+        
+        if git_ignore :
+            add_ignore(git_ignore)
+        
+        git_add()
+        
+        git_commit(project_description)
+        
+        remote_url = git_add_remote(project_name, project_description)
+        if not remote_url :
+            return False
+
+        git_connect_remote(remote_url)
+        
+        git_push()
+        
+        return True
+
+def git_init() :
+    output = subprocess.getoutput('git init')
+
+def add_ignore(git_ignore) :
+    ignore = open('./.gitignore', 'a+')
+    for file in git_ignore :
+        ignore.write(file + '\n')
+    ignore.close()
+
+
+def git_add() :
+    output = subprocess.getoutput('git add .')
+
+def git_commit(project_description) :
+    output = subprocess.getoutput(f'git commit -m "Initialised Project with Description: {project_description}"')
+
+def git_add_remote(project_name, project_description) :
+
+    print("Taking github username from git config...")
+    username = subprocess.getoutput('git config user.name')
+    if username :
+        pass
+    else :
+        username = input("Unable to take username from git config. Please enter Github Username = ")
+
+    password = input("Enter Github password: ")
+
+    repo_attributes = {
+        'name': project_name,
+        'description': project_description,
+        "homepage": "https://github.com/ishitb",
+        "private": False,
+        "has_issues": True,
+        "has_projects": True,
+        "has_wiki": True
+    }
+
+    response = requests.post(
+        'https://api.github.com/user/repos',
+        data = json.dumps(repo_attributes),
+        auth = HTTPBasicAuth(username, password)
+    )
+
+    if response.status_code == 201 :
+        return response.json()['clone_url']
+
+    else :
+        return False
+
+def git_connect_remote(remote_url) :
+    output = subprocess.getoutput(f'git remote add origin {remote_url}')
+
+def git_push() :
+    output = subprocess.getoutput('git push -u origin master')
+
+
+# For wifi automation
+import socket, time
+# from auth import SAVED_WIFI_NETWORKS
+'''
+WIFI STORED IN FORM :
+{
+    'name': "wifi name",
+    'ssid': "wifi ssid"
+}
+'''
+
+
+def check_connection() :
+    try :
+        socket.create_connection(('1.1.1.1', 53))
+        return True
+    except :
+        return False
+
+def wifi() :
+    if check_connection() :
+        return True
+    
+    else :
+        input("Please make sure you have Internet connection and then Press Enter")
+        return wifi()
