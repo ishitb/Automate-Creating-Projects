38bcd54dd8e9cb918e78679ed622b77e40b3b24c
Ishit Beswal <ib370@snu.edu.in>
2021-07-24 22:01:47 +0530

    Updated for reset pc

--- project.py
+++ project.py
@@ -6,47 +6,48 @@ from utils.custom_io import print_choices, printc, printe, inputc
 from utils.Project_Creation import native_web, react_web, react_native, flutter, django
 from utils.clear import clear
 
-BASE_PATH = 'C:/Users/Lenovo/Desktop/MyPC/Projects/'
+# BASE_PATH = 'C:/Users/Lenovo/Desktop/MyPC/Projects/'
+BASE_PATH = 'D:/Projects/'
 
 if sys.platform == 'linux' :
     BASE_PATH = os.path.join('/', 'mnt', 'c', BASE_PATH[3:])
 
 THIS_PATH = os.path.dirname(__file__)
 
-COLORS = json.load(open(os.path.join(THIS_PATH, "utils", "Shared", "colors.json")))
+COLORS = json.load(open(os.path.join(THIS_PATH, 'utils', 'Shared', 'colors.json')))
 
 MAIN_COLOR = COLORS[7]
 
-BACKGROUND_COLORS = [f"on_{color}" for color in COLORS ]
+BACKGROUND_COLORS = [f'on_{color}' for color in COLORS ]
 
 PROJECT_BASES = json.load(open(os.path.join(THIS_PATH, 'utils', 'Shared', 'project_bases.json')))
 
 IDE_CHOICES = [
     {
-        'name': "Android Studio",
+        'name': 'Android Studio',
         'command': 'studio64 . & exit'
     },
     {
-        'name': "VS Code",
+        'name': 'VS Code',
         'command': 'code . & exit'
     }
 ]
 
 def choose_project() :
 
-    printc("Project Base Options :")
+    printc('Project Base Options :')
     
     for base_index in range(len(PROJECT_BASES)) :
-        printc(f"{base_index + 1}.",' ')
+        printc(f'{base_index + 1}.',' ')
         cprint(PROJECT_BASES[base_index]['name'], PROJECT_BASES[base_index]['color'])
     
-    choice = int(inputc("Choose your Project Base (Enter a Number)"))
+    choice = int(inputc('Choose your Project Base (Enter a Number)'))
 
     while(True) :
         if choice in list(range(1, len(PROJECT_BASES) + 1)) :
             break
         else :
-            choice = int(inputc("Please choose a valid Project Number", 'red', ['bold']))
+            choice = int(inputc('Please choose a valid Project Number', 'red', ['bold']))
 
     return (PROJECT_BASES[choice - 1]['name'], PROJECT_BASES[choice - 1]['directory'], PROJECT_BASES[choice - 1]['color'])
 
@@ -64,16 +65,16 @@ class Project :
 
         self.PROJECT_BASE, self.PROJECT_DIR, self.PROJECT_COLOR = base, directory, color
  
-        self.PROJECT_NAME = inputc("Please Enter Project Name")
+        self.PROJECT_NAME = inputc('Please Enter Project Name')
         self.format_project_name()
         print()
 
-        self.PROJECT_DESCRIPTION = inputc("Please provide a one-line desciption for the Project (Leave Blank if None)", newLine=True)
+        self.PROJECT_DESCRIPTION = inputc('Please provide a one-line desciption for the Project (Leave Blank if None)', newLine=True)
 
     def format_project_name(self, flutter=False) :
         self.PROJECT_NAME = self.PROJECT_NAME.replace(' ', '_')
         self.PROJECT_NAME = self.PROJECT_NAME.lower() if flutter else self.PROJECT_NAME
-        printc("Your Project Name is", end=' ')
+        printc('Your Project Name is', end=' ')
         cprint(self.PROJECT_NAME, self.PROJECT_COLOR)
 
     def add_readme(self) :
@@ -92,10 +93,10 @@ class Project :
     def add_to_github(self, git_ignore) :
         git_confirm = push_to_github(self.PROJECT_NAME, self.PROJECT_DESCRIPTION, git_ignore)
         if git_confirm :
-            printc("Added to Github Successfully")
+            printc('Added to Github Successfully')
 
         else :
-            printe("Failed to add to Github! Please check your network connection and then try adding to github manually!")
+            printe('Failed to add to Github! Please check your network connection and then try adding to github manually!')
 
     def start_flutter_project(self) :
         return flutter.build(self.PROJECT_NAME, IDE_CHOICES)
@@ -121,18 +122,18 @@ if __name__ == '__main__' :
     os.chdir(BASE_PATH)
 
     print()
-    cprint(" N E W ", 'magenta', 'on_white', end='', attrs=['bold'])
-    cprint(" P R O J E C T ", 'white', 'on_magenta', attrs=['bold'])
+    cprint(' N E W ', 'magenta', 'on_white', end='', attrs=['bold'])
+    cprint(' P R O J E C T ', 'white', 'on_magenta', attrs=['bold'])
     print()
 
     base, directory, color = choose_project()
 
     print()
-    printc("Your Project Base is", end=' ')
+    printc('Your Project Base is', end=' ')
     cprint(base, color)
     print()
 
-    if base != "Competitive Programming" :
+    if base != 'Competitive Programming' :
         os.chdir(directory)
 
         project = Project(base, directory, color)
@@ -154,14 +155,14 @@ if __name__ == '__main__' :
                   
         project.add_readme()
 
-        github_choice = print_choices("Do you want to add the project to remote Github Repository or keep it Local", choices=["Remote Github Repository", "Keep it Local"])
+        github_choice = print_choices('Do you want to add the project to remote Github Repository or keep it Local', choices=['Remote Github Repository', 'Keep it Local'])
         if github_choice == 1 :
-            printc("Adding to Github...")
+            printc('Adding to Github...')
             project.add_to_github(git_ignore)
             clear()
 
         else :
-            printc("Keeping the Project Local and", end=' ')
+            printc('Keeping the Project Local and', end=' ')
 
         open_ide(selected_ide)
 
